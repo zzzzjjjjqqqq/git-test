e867e588dcdc1bd3b77a383ef5c7556448008d59
zjq <1783142247@qq.com>
2023-09-12 10:17:22 +0800

    hot-fix first idea

--- src/main/java/com/atzjq/git/GitTest.java
+++ src/main/java/com/atzjq/git/GitTest.java
@@ -4,5 +4,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git1");
+        System.out.println("hello git4");
     }
 }
